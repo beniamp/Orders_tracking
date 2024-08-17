4491296824a931c2bbe347a057f07535d85ee221
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-17 15:49:22 +0330

    Update code_OrderTracking.py

--- code_OrderTracking.py
+++ code_OrderTracking.py
@@ -17,11 +17,10 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
-
-# Row A
-a2, a3, a4 = st.columns(3)
 image = Image.open('dgland_icon.png')
 st.image(image, width=100)  # Change 100 to the desired width in pixels
+# Row A
+a2, a3, a4 = st.columns(3)
 a2.metric("Wind", "9 mph", "-8%")
 a3.metric("Humidity", "86%", "4%")
 a4.metric("Temperture", "33", "3%")
