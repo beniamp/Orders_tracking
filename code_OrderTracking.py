f428f191582141ac77f99f922f805f692be40b16
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-17 15:08:32 +0330

    Update code_OrderTracking.py

--- code_OrderTracking.py
+++ code_OrderTracking.py
@@ -4,7 +4,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import io
-from xlsxwriter import Workbook
 import numpy as np
 #import pyodbc as odbc
 
