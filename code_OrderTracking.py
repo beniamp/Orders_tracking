0aabc7c096ad60f28f233051080221244403633c
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-18 11:47:09 +0330

    Update code_OrderTracking.py

--- code_OrderTracking.py
+++ code_OrderTracking.py
@@ -75,15 +75,15 @@ past_7_days = past_8_days[1:]
 
 # Get the 7 days before the last 7 days
 if len(past_7_days) >= 7:
-    2ndpast_7_days = get_past_dates(past_8_days[0], days=7)
+    second_past_7_days = get_past_dates(past_8_days[0], days=7)
 else:
-    2ndpast_7_days = []
+    second_past_7_days = []
 
-past_14_days = 2ndpast_7_days + past_7_days
+past_14_days = second_past_7_days + past_7_days
 
 # Filter DataFrames
 df_current_week = filtered_df[filtered_df['Date_Formatted'].isin(past_7_days)]
-df_previous_week = filtered_df[filtered_df['Date_Formatted'].isin(2ndpast_7_days)]
+df_previous_week = filtered_df[filtered_df['Date_Formatted'].isin(second_past_7_days)]
 
 # Calculate metrics for current week
 current_total_sales = df_current_week['TotalPrice'].sum()
@@ -115,7 +115,7 @@ else:
     st.write("Calculated Weekdays: Not enough data to display")
 
 if 2ndpast_7_days:
-    st.write(f"Previous Weekdays: From {2ndpast_7_days[0]} to {2ndpast_7_days[-1]}")
+    st.write(f"Previous Weekdays: From {second_past_7_days[0]} to {second_past_7_days[-1]}")
 else:
     st.write("Previous Weekdays: Not enough data to display")
 
