75515081d4d4057b2b9b446720e9acc236c63d9c
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-24 13:12:26 +0330

    Update code_OrderTracking.py

--- code_OrderTracking.py
+++ code_OrderTracking.py
@@ -329,7 +329,7 @@ if product_quantities_by_range:
     # Display the summary table
     st.write("Total Quantity by Product for Each Date Range")
     st.write(summary_df)
-    count_products = summary_df.shape()
+    count_products = summary_df.shape[0]
     st.write(count_products)
 else:
     st.write("No valid date ranges with data found.")
